53f8d0dfecf46232e75df8268ddd9f6c2b533150
Yoan Blanc <yoan@dosimple.ch>
2016-10-27 18:47:30 +0200

    oOops

--- 02-RNG/src/main/java/ch/masrad/xml/rng/Main.java
+++ 02-RNG/src/main/java/ch/masrad/xml/rng/Main.java
@@ -5,7 +5,7 @@ package ch.masrad.xml.rng;
 public class Main {
 	public static void main(String args[]) throws Exception {
 		ValidateXML.validate("document.xml", "document.rnc");
-		ConvertSchema.convertXsd("document.rnc", "document.rng");
+		ConvertSchema.convertRng("document.rnc", "document.rng");
 		ConvertSchema.convertXsd("document.rnc", "document.xsd");
 		ConvertSchema.convertDtd("document.rnc", "document.dtd");
 	}
